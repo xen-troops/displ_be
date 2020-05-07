xen-troops/displ_be

commit 6bcf9acb219e474d42aef5c57ce5d2be895f7b48

Handle buffer offset passed with XENDISPL_OP_DBUF_CREATE v2
There are cases when display data buffer is created with non-zero
offset to the data start. Handle such cases and also provide offset
while creating a display buffer.
Handle new buffer offset parameter passed with XENDISPL_OP_DBUF_CREATE
version 2.

Note! There is a dependency on displif protocol support for data buffer
offset field which is available in protocol versions starting 2.

Signed-off-by: Oleksandr Andrushchenko <oleksandr_andrushchenko@epam.com>
Reviewed-by: Oleksandr Grytsov <oleksandr_grytsov@epam.com>
Reviewed-by: Volodymyr Babchuk <volodymyr_babchuk@epam.com>

src/displayBackend/DisplayCommandHandler.cpp
@@ -24,6 +24,7 @@
 
 #include <xen/be/Exception.hpp>
 
+using std::dec;
 using std::hex;
 using std::setfill;
 using std::setw;
@@ -31,6 +32,16 @@ using std::unordered_map;
 
 using DisplayItf::ConnectorPtr;
 using DisplayItf::DisplayPtr;
+
+/**
+ * displif protocol version 1 header had defined the version as a string,
+ * so it cannot be used with the preprocessor. Work this around by
+ * re-defining the version as an integer.
+ */
+#ifndef WITH_DISPLIF_INT_VERSION
+#undef XENDISPL_PROTOCOL_VERSION
+#define XENDISPL_PROTOCOL_VERSION	1
+#endif
 
 unordered_map<int, DisplayCommandHandler::CommandFn>
 	DisplayCommandHandler::sCmdTable =
@@ -157,13 +168,34 @@ void DisplayCommandHandler::createDisplayBuffer(const xendispl_req& req)
 
 	DLOG(mLog, DEBUG) << "Handle command [CREATE DBUF], cookie: "
 					  << hex << setfill('0') << setw(16)
-					  << dbufReq->dbuf_cookie;
+					  << dbufReq->dbuf_cookie
+					  << ", offset: " << dec << dbufReq->data_ofs;
 
 	bool beAllocRefs = dbufReq->flags & XENDISPL_DBUF_FLG_REQ_ALLOC;
+
+	/**
+	 * Check if offset is supported by the displif protocol
+	 * and if libxenbe does the same.
+	 * Special handling is required for be_alloc == 1 mode which
+	 * is not supported with a non-zero offsets: do not let us down to
+	 * creating any resources and terminate the command here.
+	 */
+#if (XENDISPL_PROTOCOL_VERSION >= 2)
+	size_t data_ofs = dbufReq->data_ofs;
+#else
+	size_t data_ofs = 0;
+#endif
+
+	if (beAllocRefs && data_ofs)
+	{
+		throw XenBackend::Exception("Can't create buffer with non-zero offset in this mode",
+									EINVAL);
+	}
 
 	mBuffersStorage->createDisplayBuffer(dbufReq->dbuf_cookie,
 										 beAllocRefs,
 										 dbufReq->gref_directory,
+										 data_ofs,
 										 dbufReq->buffer_sz,
 										 dbufReq->width, dbufReq->height,
 										 dbufReq->bpp);

CMakeLists.txt
@@ -56,6 +56,22 @@ if(NOT WITH_DRM AND WITH_ZCOPY)
 endif()
 
 ################################################################################
+# Try compile tests
+################################################################################
+
+# Older displif protocol header had its version defined as a string,
+# so we need a check if we can safely test protocol version with a cpp
+# preprocessor
+try_compile(WITH_DISPLIF_INT_VERSION
+	"${CMAKE_BINARY_DIR}/cmake"
+	"${CMAKE_SOURCE_DIR}/cmake/compile_test_displif_v1.cpp"
+)
+
+if(WITH_DISPLIF_INT_VERSION)
+	add_definitions(-DWITH_DISPLIF_INT_VERSION)
+endif()
+
+################################################################################
 # Compiler flags
 ################################################################################
 

cmake/compile_test_displif_v1.cpp
@@ -1,0 +1,18 @@
+#include <stdint.h>
+
+extern "C" {
+#include <xen/io/displif.h>
+}
+
+/**
+ * Older displif protocol header had its version defined as a string,
+ * so we need a check if we can safely test protocol version with a cpp
+ * preprocessor: if the below fails it means that XENDISPL_PROTOCOL_VERSION
+ * is defined as a string "1".
+ */
+int main(int argc, char **argv)
+{
+#if XENDISPL_PROTOCOL_VERSION >= 2
+#endif
+	return 0;
+}

src/displayBackend/BuffersStorage.cpp
@@ -69,7 +69,7 @@ BuffersStorage::~BuffersStorage()
 
 void BuffersStorage::createDisplayBuffer(uint64_t dbCookie, bool beAllocRefs,
 										 grant_ref_t startDirectory,
-										 uint32_t size,
+										 size_t offset, uint32_t size,
 										 uint32_t width, uint32_t height,
 										 uint32_t bpp)
 {
@@ -92,23 +92,25 @@ void BuffersStorage::createDisplayBuffer(uint64_t dbCookie, bool beAllocRefs,
 
 		DLOG(mLog, DEBUG) << "Create pending display buffer, start dir: "
 						  << startDirectory
-						  << ", size: " << size << ", DB cookie: 0x"
+						  << ", size: " << size << ", offset: " << offset
+						  << ", DB cookie: 0x"
 						  << hex << setfill('0') << setw(16)
 						  << dbCookie;
 
-		mPendingDisplayBuffers.emplace(dbCookie, refs);
+		mPendingDisplayBuffers.emplace(dbCookie, PendingBuffer{offset, refs});
 	}
 	else
 	{
 		DLOG(mLog, DEBUG) << "Create display buffer, w: "
 						  << width << ", h: " << height << ", bpp: " << bpp
+						  << ", offset: " << offset
 						  << ", start dir: " << startDirectory
 						  << ", size: " << size << ", DB cookie: 0x"
 						  << hex << setfill('0') << setw(16)
 						  << dbCookie;
 
 		auto displayBuffer = mDisplay->createDisplayBuffer(width, height, bpp,
-														   mDomId, refs,
+														   offset, mDomId, refs,
 														   beAllocRefs);
 
 		mDisplayBuffers.emplace(dbCookie, displayBuffer);
@@ -260,12 +262,15 @@ void BuffersStorage::handlePendingDisplayBuffers(uint64_t dbCookie,
 
 		DLOG(mLog, DEBUG) << "Create display buffer from pending, w: "
 						  << width << ", h: " << height << ", bpp: " << bpp
+						  << ", offset: " << iter->second.offset
 						  << ", DB cookie: 0x"
 						  << hex << setfill('0') << setw(16)
 						  << dbCookie;
 
 		auto displayBuffer = mDisplay->createDisplayBuffer(width, height, bpp,
-														   mDomId, iter->second,
+														   iter->second.offset,
+														   mDomId,
+														   iter->second.refs,
 														   false);
 
 		mDisplayBuffers.emplace(dbCookie, displayBuffer);

src/displayBackend/BuffersStorage.hpp
@@ -56,14 +56,17 @@ public:
 	 * @param dbCookie       display buffer cookie
 	 * @param beAllocRefs    indicates that backend shall allocate refs
 	 * @param startDirectory grant table reference to the buffer start directory
+	 * @param offset         offset of the data in the buffer
 	 * @param size           buffer size
 	 * @param width          width in pixels
 	 * @param height         height in pixels
 	 * @param bpp            bits per pixel
 	 */
 	void createDisplayBuffer(uint64_t dbCookie, bool beAllocRefs,
-							 grant_ref_t startDirectory, uint32_t size,
-							 uint32_t width, uint32_t height, uint32_t bpp);
+							 grant_ref_t startDirectory,
+							 size_t offset, uint32_t size,
+							 uint32_t width, uint32_t height,
+							 uint32_t bpp);
 
 	/**
 	 * Creates frame buffer
@@ -109,9 +112,14 @@ private:
 
 	std::mutex mMutex;
 
+	struct PendingBuffer {
+		size_t offset;
+		DisplayItf::GrantRefs refs;
+	};
+
 	std::unordered_map<uint64_t, DisplayItf::FrameBufferPtr> mFrameBuffers;
 	std::unordered_map<uint64_t, DisplayItf::DisplayBufferPtr> mDisplayBuffers;
-	std::unordered_map<uint64_t, DisplayItf::GrantRefs> mPendingDisplayBuffers;
+	std::unordered_map<uint64_t, PendingBuffer> mPendingDisplayBuffers;
 
 	uint32_t getBpp(uint32_t format);
 	void handlePendingDisplayBuffers(uint64_t dbCookie, uint32_t width,

src/displayBackend/DisplayItf.hpp
@@ -220,16 +220,19 @@ public:
 	 * @param width  width
 	 * @param height height
 	 * @param bpp    bits per pixel
+	 * @param offset offset of the data in the buffer
 	 * @return shared pointer to the display buffer
 	 */
 	virtual DisplayBufferPtr createDisplayBuffer(
-			uint32_t width, uint32_t height, uint32_t bpp) = 0;
+			uint32_t width, uint32_t height, uint32_t bpp,
+			size_t offset) = 0;
 
 	/**
 	 * Creates display buffer with associated grant table buffer
 	 * @param width     width
 	 * @param height    height
 	 * @param bpp       bits per pixel
+	 * @param offset    offset of the data in the buffer
 	 * @param domId     domain ID
 	 * @param refs      vector of grant table reference
 	 * @param allocRefs indicates that grant refs should be allocated on
@@ -237,7 +240,7 @@ public:
 	 * @return shared pointer to the display buffer
 	 */
 	virtual DisplayBufferPtr createDisplayBuffer(
-			uint32_t width, uint32_t height, uint32_t bpp,
+			uint32_t width, uint32_t height, uint32_t bpp, size_t offset,
 			uint16_t domId, GrantRefs& refs, bool allocRefs) = 0;
 
 	/**

src/displayBackend/drm/Display.cpp
@@ -193,17 +193,17 @@ DisplayItf::ConnectorPtr Display::createConnector(const string& name)
 }
 
 DisplayBufferPtr Display::createDisplayBuffer(uint32_t width, uint32_t height,
-											 uint32_t bpp)
+											 uint32_t bpp, size_t offset)
 {
 	lock_guard<mutex> lock(mMutex);
 
 	LOG(mLog, DEBUG) << "Create display buffer";
 
-	return DisplayBufferPtr(new DumbDrm(mDrmFd, width, height, bpp));
+	return DisplayBufferPtr(new DumbDrm(mDrmFd, width, height, bpp, offset));
 }
 
 DisplayBufferPtr Display::createDisplayBuffer(
-		uint32_t width, uint32_t height, uint32_t bpp,
+		uint32_t width, uint32_t height, uint32_t bpp, size_t offset,
 		domid_t domId, DisplayItf::GrantRefs& refs, bool allocRefs)
 {
 	lock_guard<mutex> lock(mMutex);
@@ -219,7 +219,7 @@ DisplayBufferPtr Display::createDisplayBuffer(
 	}
 
 	return DisplayBufferPtr(new DumbZCopyFrontDrm(mDrmFd,
-												  width, height, bpp,
+												  width, height, bpp, offset,
 												  domId, refs));
 #else
 	LOG(mLog, DEBUG) << "Create display buffer";
@@ -229,7 +229,7 @@ DisplayBufferPtr Display::createDisplayBuffer(
 		throw  Exception("Can't allocate refs: ZCopy disabled", EINVAL);
 	}
 
-	return DisplayBufferPtr(new DumbDrm(mDrmFd, width, height, bpp,
+	return DisplayBufferPtr(new DumbDrm(mDrmFd, width, height, bpp, offset,
 										domId, refs));
 #endif
 }
@@ -311,7 +311,7 @@ void Display::handleFlipEvent(int fd, unsigned int sequence,
 
 #if defined(WITH_WAYLAND) && defined(WITH_ZCOPY)
 DisplayBufferPtr DisplayWayland::createDisplayBuffer(
-		uint32_t width, uint32_t height, uint32_t bpp,
+		uint32_t width, uint32_t height, uint32_t bpp, size_t offset,
 		domid_t domId, GrantRefs& refs, bool allocRefs)
 {
 	lock_guard<mutex> lock(mMutex);
@@ -326,7 +326,7 @@ DisplayBufferPtr DisplayWayland::createDisplayBuffer(
 	}
 
 	return DisplayBufferPtr(new DumbZCopyFront(mDrmFd,
-											   width, height, bpp,
+											   width, height, bpp, offset,
 											   domId, refs));
 }
 #endif

src/displayBackend/drm/Display.hpp
@@ -85,22 +85,25 @@ public:
 	 * @param width  width
 	 * @param height height
 	 * @param bpp    bits per pixel
+	 * @param offset offset of the data in the buffer
 	 * @return shared pointer to the display buffer
 	 */
 	DisplayItf::DisplayBufferPtr createDisplayBuffer(
-			uint32_t width, uint32_t height, uint32_t bpp) override;
+			uint32_t width, uint32_t height, uint32_t bpp,
+			size_t offset) override;
 
 	/**
 	 * Creates display buffer with associated grand table buffer
 	 * @param width  width
 	 * @param height height
 	 * @param bpp    bits per pixel
+	 * @param offset offset of the data in the buffer
 	 * @param domId  domain id
 	 * @param refs   grant table references
 	 * @return shared pointer to the display buffer
 	 */
 	DisplayItf::DisplayBufferPtr createDisplayBuffer(
-			uint32_t width, uint32_t height, uint32_t bpp,
+			uint32_t width, uint32_t height, uint32_t bpp, size_t offset,
 			domid_t domId, DisplayItf::GrantRefs& refs,
 			bool allocRefs) override;
 
@@ -160,12 +163,13 @@ public:
 	 * @param width  width
 	 * @param height height
 	 * @param bpp    bits per pixel
+	 * @param offset offset of the data in the buffer
 	 * @param domId  domain id
 	 * @param refs   grant table references
 	 * @return shared pointer to the display buffer
 	 */
 	DisplayItf::DisplayBufferPtr createDisplayBuffer(
-			uint32_t width, uint32_t height, uint32_t bpp,
+			uint32_t width, uint32_t height, uint32_t bpp, size_t offset,
 			domid_t domId, DisplayItf::GrantRefs& refs,
 			bool allocRefs) override;
 };

commit c503b27c6fd8256b9113da46eb5b1d3efba88c81

Fix compile error for XENDISPL_OP_DBUF_CREATE before v2
Fixes: 6bcf9acb219e ("Handle buffer offset passed with XENDISPL_OP_DBUF_CREATE v2")

Signed-off-by: Oleksandr Andrushchenko <oleksandr_andrushchenko@epam.com>
Reviewed-by: Oleksandr Tyshchenko <oleksandr_tyshchenko@epam.com>
Reviewed-by: Andrii Chepurnyi <andrii_chepurnyi@epam.com>

src/displayBackend/DisplayCommandHandler.cpp
@@ -166,11 +166,6 @@ void DisplayCommandHandler::createDisplayBuffer(const xendispl_req& req)
 {
 	const xendispl_dbuf_create_req* dbufReq = &req.op.dbuf_create;
 
-	DLOG(mLog, DEBUG) << "Handle command [CREATE DBUF], cookie: "
-					  << hex << setfill('0') << setw(16)
-					  << dbufReq->dbuf_cookie
-					  << ", offset: " << dec << dbufReq->data_ofs;
-
 	bool beAllocRefs = dbufReq->flags & XENDISPL_DBUF_FLG_REQ_ALLOC;
 
 	/**
@@ -185,6 +180,11 @@ void DisplayCommandHandler::createDisplayBuffer(const xendispl_req& req)
 #else
 	size_t data_ofs = 0;
 #endif
+
+	DLOG(mLog, DEBUG) << "Handle command [CREATE DBUF], cookie: "
+					  << hex << setfill('0') << setw(16)
+					  << dbufReq->dbuf_cookie
+					  << ", offset: " << dec << data_ofs;
 
 	if (beAllocRefs && data_ofs)
 	{